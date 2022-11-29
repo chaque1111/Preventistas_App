211cf6a26862664d3f367be1af60d96d6a0180b1
chaque1111 <99604927+chaque1111@users.noreply.github.com>
2022-11-29 18:01:26 -0300

    agregando table

---
+++ api/src/models/Cliente.js
@@ -0,0 +1,169 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('cliente', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    rzsocial:{ 
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    nomFant:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    direccion:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    localidad: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    provincia: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    pais: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    zona: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    whatsapp: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    deudas: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true,
+      defaultValue: false,
+    },
+    tipoDocument: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    numDocument: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    condicionIva: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    categorias: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    nombreVendedor: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    saldo: {
+      type: DataTypes.DECIMAL(13,2),
+      allowNull: true,
+      defaultValue: null
+    },
+    observaciones: {
+      type: DataTypes.STRING(1000),
+      allowNull: true
+    },
+    contacto: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    listaPrecios: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    condVta: {
+    type: DataTypes.STRING,
+    allowNull: true
+   },
+   credito: {
+     type: DataTypes.INTEGER,
+     allowNull: true,
+   },
+   bonif: {
+     type: DataTypes.STRING,
+     allowNull: true,
+   },
+   abasto: {
+     type: DataTypes.BOOLEAN,
+     allowNull: true,
+     defaultValue: false,
+   },
+   percIBTasa: {
+     type: DataTypes.DECIMAL(4,2),
+     allowNull: true
+   },
+   LeyF: {
+       type: DataTypes.STRING,
+       allowNull: true,
+   },
+   LeyR: {
+       type: DataTypes.STRING,
+       allowNull: true,
+   },
+   activo: {
+     type: DataTypes.BOOLEAN,
+     allowNull: false,
+     defaultValue: true,
+   },
+   fechaUC:{
+     type: DataTypes.STRING,
+     allowNull: false,
+   },
+   actLista: {
+     type: DataTypes.BOOLEAN,
+     allowNull: false,
+     defaultValue: true
+   },
+   email: {
+     type: DataTypes.STRING,
+     allowNull: true,
+   },
+   web: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  llamar: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  pasar: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  observCobranza:{
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+   fechaAlta:{
+     type: DataTypes.STRING,
+     allowNull: true
+   },
+   fechaRepos: {
+     type: DataTypes.STRING,
+     allowNull: true,
+   },
+   nroProv: {
+     type: DataTypes.STRING,
+     allowNull: true,
+   },
+   lugarEntrega:{
+     type: DataTypes.STRING,
+     allowNull: true,
+   }
+  });
+};
