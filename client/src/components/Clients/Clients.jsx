b62d39c4d39c5f0b1f8ffc85e4484122924de67d
Enrique Martín Garay <98557235+Quique40@users.noreply.github.com>
2022-12-12 17:40:26 -0300

    Distribution of client component cards

--- client/src/components/Clients/Clients.jsx
+++ client/src/components/Clients/Clients.jsx
@@ -18,10 +18,11 @@ export default function Clients() {
     return(
         <div className={styles.container}>
             <div className={styles.cardsCont}>
+                <div className= {styles.cardsComponent} >
            {clients.length > 0 ? (
             clients.map((e) => {
               return (
-                <div key={e.id} >
+                <div key={e.id} className={styles.singleCard}>
                   {/* <Link to={"/detail/" + e.id}> */}
                     <Card name={e.name} localidad={e.localidad} vendedor={e.nombreVendedor} zona={e.zona}/>
                   {/* </Link> */}
@@ -31,6 +32,7 @@ export default function Clients() {
             })
           ) : 
           <p>...Loading</p>}
+          </div>
           </div>
         </div>
     )
